524a56c16a93031e505155c24ee8cca50ed1f93a
shivkant kurmi <pshivkantpatel@gmail.com>
2026-08-08 17:00:08 +0530

    Fix production CORS and authentication cookie

--- backend/server.js
+++ backend/server.js
@@ -14,10 +14,11 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
-      "https://right-engineering-works-rg9jqd5cb-shivkant-kurmi.vercel.app",
-    ],
-    credentials: true,
+  "http://localhost:5173",
+  "https://right-engineering-works-rg9jqd5cb-shivkant-kurmi.vercel.app",
+  "https://right-engineering-works-2p968bwvy-shivkant-kurmi.vercel.app",
+],
+credentials: true,
   })
 );
 
