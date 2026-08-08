06a04e52ecb33e04a8575a22b06701801e1dbd12
shivkant kurmi <pshivkantpatel@gmail.com>
2026-08-08 15:30:53 +0530

    Fix production CORS

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://right-engineering-works-rg9jqd5cb-shivkant-kurmi.vercel.app",
+    ],
     credentials: true,
   })
 );
